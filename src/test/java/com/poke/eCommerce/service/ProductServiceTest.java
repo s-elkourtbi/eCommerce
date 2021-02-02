e9219369a5d6b2b8da4646ab2393eff15b3ba632
soufiane <S.elkourtbi00!>
2021-02-02 21:05:35 +0100

    TU Product Service

--- src/test/java/com/poke/eCommerce/service/ProductServiceTest.java
+++ src/test/java/com/poke/eCommerce/service/ProductServiceTest.java
@@ -2,17 +2,21 @@ package com.poke.eCommerce.service;
 
 import com.poke.eCommerce.ECommerceApplication;
 import com.poke.eCommerce.entities.Product;
+import com.poke.eCommerce.valueObject.ProductVO;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.hamcrest.CoreMatchers.is;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
-//@RunWith(SpringRunner.class)
+
 @SpringBootTest(classes = ECommerceApplication.class)
 @WebAppConfiguration
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
@@ -21,18 +25,58 @@ public class ProductServiceTest {
     @Autowired
     ProductService productService;
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+    @Test
+    public void findProductByIdTest(){
+        //given
+        Long idProduct = 1L;
+        //when
+        ProductVO productVO = productService.findProductById(idProduct);
+        //then
+        Assert.assertTrue(productVO.getIdProduct() == 1L);
+    }
 
 
-//    @Test
-//    public void getOrderById_shouldGiveCorrespondigOrder(){
-//        //given
-//        Long idProduct = 1l;
-//        //when
-//        Product product = productService.findProductById(idProduct);
-//        //then
-//        assertThat(order.getId(), is(1l));
-//    }
+    @Test
+    public void findProductByNameTest() {
+        //given
+        String name = "Radio";
+        //when
+        ProductVO productVO = productService.findProductByName("Radio");
+        //then
+        Assert.assertTrue(productVO.getName().equals("Radio"));
+    }
+
+    @Test
+    public void findAllProductTest() {
+
+        //when
+        List<Product> findAll = productService.findAllProduct();
+        //then
+        Assert.assertTrue(findAll.size() == 5);
+    }
+    @Test
+    public void deletProductByIDTest() {
+        //given
+        Long id = 4L;
+        //when
+        boolean deletProductByID = productService.deletProductByID(id);
+        //then
+        Assert.assertTrue(deletProductByID);
+    }
+
+    @Test
+    public void SortProductByPriceTest() {
+
+        //when
+        List<Product> sortedList1 = productService.sortProduct("price");
+        List<Product> sortedList2 = productService.findAllProduct().stream()
+                .sorted(Comparator.comparing(Product::getPrice))
+                .collect(Collectors.toList());
+        //then
+        Assert.assertEquals(sortedList1.get(0).getPrice(), sortedList2.get(0).getPrice());
+        Assert.assertEquals(sortedList1.get(1).getPrice(), sortedList2.get(1).getPrice());
+        Assert.assertEquals(sortedList1.get(2).getPrice(), sortedList2.get(2).getPrice());
+        Assert.assertEquals(sortedList1.get(3).getPrice(), sortedList2.get(3).getPrice());
+    }
 
 }
